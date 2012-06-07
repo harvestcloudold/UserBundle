f7d964c965d1349674cdffd016fdddd77242b2ff
Tom Haskins-Vaughan <tom@harvestcloud.com>
2012-06-06 20:06:35 -0400

    Committing default bundle files

---
+++ HarvestCloudUserBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the Harvest Cloud package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HarvestCloud\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HarvestCloudUserBundle extends Bundle
+{
+}
