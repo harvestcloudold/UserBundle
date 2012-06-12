e3cd68dc075205f2b5fa12d0e55789794a6ebe16
Tom Haskins-Vaughan <tom@harvestcloud.com>
2012-06-11 21:22:16 -0400

    Overriding FODUserBundle login view

--- HarvestCloudUserBundle.php
+++ HarvestCloudUserBundle.php
@@ -13,4 +13,18 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class HarvestCloudUserBundle extends Bundle
 {
+    /**
+     * Declare HarvestCloud\UserBundle as child of FOS\UserBundle
+     * This will allow us to override the default views, as documented
+     * in FOS\UserBundle documentation
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @sicne  2012-06-08
+     *
+     * @return string
+     */
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
