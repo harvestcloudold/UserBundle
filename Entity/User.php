aa1a71e990fd977bf5fafe7ca4e1a42caf61098d
Tom Haskins-Vaughan <tom@harvestcloud.com>
2012-06-13 15:35:28 -0400

    bugfix: namespace of Location not supplied in Harvestcloud User

--- Entity/User.php
+++ Entity/User.php
@@ -13,6 +13,7 @@ use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use HarvestCloud\CoreBundle\Entity\Profile;
+use HarvestCloud\CoreBundle\Entity\Location;
 
 /**
  * @ORM\Entity
