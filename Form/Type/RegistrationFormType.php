541a90bb9d462456f1117527b1e9716ab9c18458
Tom Haskins-Vaughan <tom@harvestcloud.com>
2013-02-02 16:06:52 -0500

    Using FormBuilderInterface instead of FormBuilder for  type

--- Form/Type/RegistrationFormType.php
+++ Form/Type/RegistrationFormType.php
@@ -2,7 +2,7 @@
 
 namespace HarvestCloud\UserBundle\Form\Type;
 
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
 
 /**
@@ -22,7 +22,7 @@ class RegistrationFormType extends BaseType
      * @param  FormBuilder  $builder
      * @param  array        $options
      */
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
 
